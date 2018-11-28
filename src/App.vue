6825a860458e26abb54789ea9fd9fbf15d62338c
possibly1 <brad.clonan@hotmail.com>
2018-11-28 00:55:10 -0500

    add to github

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <dev-menu @toggleMenu="toggleMenu" :menuToggled="menuToggled" :NavItems="NavItems" />
+    <button @click="toggleMenu">Dev Menu</button>
+  </div>
+</template>
+
+<script>
+  import DeveloperControl from "@/components/DeveloperControl.vue";
+  export default {
+    name: "app",
+    data() {
+      return {
+        activeMenu: false,
+        NavItems: [
+          {
+            link: "https://elegant-jackson-350189.netlify.com/",
+            destination: "Developer Documents"
+          },
+          {
+            link: "https://github.com/possibly1/DistanceCalc",
+            destination: "Calculator Repository"
+          },
+          {
+            link: "https://github.com/possibly1/distanceDocs",
+            destination: "Docs Repository"
+          },
+
+          {
+            link: "https://github.com/possibly1/distanceWidget",
+            destination: "Widget Repository"
+          }
+        ]
+      };
+    },
+    components: {
+      "dev-menu": DeveloperControl
+    },
+    methods: {
+      toggleMenu() {
+        this.activeMenu = !this.activeMenu;
+      }
+    },
+    computed: {
+      menuToggled() {
+        return this.activeMenu;
+      }
+    }
+  };
+</script>
+
+<style>
+  body {
+    font-family: "Lato", sans-serif;
+  }
+</style>
---
+++ src/components/DeveloperControl.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="sidenav" v-if="menuToggled === true" :class="[ menuToggled === true ? 'is-active' : '']">
+    <a href="javascript:void(0)" class="closebtn" @click.prevent="toggleMenu">&times;</a>
+    <h1 class="titleBlock">Destinations</h1>
+    <a v-for="item in NavItems" :key="item.id" :href="item.link">{{item.destination}}</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DeveloperControler",
+    props: ["menuToggled", "NavItems"],
+    methods: {
+      toggleMenu() {
+        this.$emit("toggleMenu");
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: blueviolet;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+  }
+
+  .sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 1.2rem;
+    color: #f1f1f1;
+    display: block;
+    transition: 0.3s;
+  }
+
+  .sidenav a:hover {
+    color: #000000;
+  }
+
+  .sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+  }
+
+  #main {
+    transition: margin-left 0.5s;
+    padding: 16px;
+  }
+
+  @media screen and (max-height: 450px) {
+    .sidenav {
+      padding-top: 15px;
+    }
+    .sidenav a {
+      font-size: 18px;
+    }
+  }
+
+  .is-active {
+    width: 250px;
+  }
+  .titleBlock {
+    text-align: center;
+    text-decoration: underline;
+    color: black;
+  }
+</style>
